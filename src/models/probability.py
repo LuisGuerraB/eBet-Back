fd87548857dd5c21b303243c280beab49637e313
LuisGuerraB <luisguebat@hotmail.com>
2023-07-06 14:11:31 +0200

    Update Scheduler.update_result logic 3

--- src/models/probability.py
+++ src/models/probability.py
@@ -66,7 +66,7 @@ class Probability(db.Model):
         matches = [play.match for play in plays if play.match is not None]
         if len(results) != 0:
             prob_finished_early = ProbUnit.calc_prob(
-                [1 if match.get_final_number_of_sets is not None and match.sets > match.get_final_number_of_sets else 0
+                [1 if match.get_final_number_of_sets() is not None and match.sets > match.get_final_number_of_sets() else 0
                  for match in matches if match.sets != 1], force=True)
             if probability is None:
                 probability = Probability(team_id=team_id, league_id=league_id)
